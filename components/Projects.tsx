3fffa131f397b5ef54f4f035a11a32d94e11d006
Ramon Santos <ramoneduardo2000@live.com>
2026-05-18 12:11:32 -0300

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,7 +11,6 @@ const projects = [
         description: "Novo portfólio criado em Next.js e Tailwind CSS, com o objetivo de praticar o uso do Tailwind CSS e criar um portfólio mais moderno e leve. Projeto criado com foco em aprendizagem.",
         tags: ["Next.js", "Tailwind CSS", "JavaScript", "Internacionalização", "Responsive", "Fetch API",],
         github: "https://github.com/Ramon-Eduardo/Portfolio",
-        demo: "#",
         color: "from-violet-500 to-purple-500",
         image: "/portfolio.png"
     },
