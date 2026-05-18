9d8ae28e13d24adf94aff0d4ca451e6167f0bc82
Ramon Santos <ramoneduardo2000@live.com>
2026-05-18 01:12:46 -0300

    fixed api

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 const nodemailer = require("nodemailer")
 
 export async function POST(request: Request) {
-    
     try {
         const body = await request.json();
 
@@ -23,44 +22,42 @@ export async function POST(request: Request) {
             );
         }
 
-    const message = {
-        from: `${process.env.EMAIL_FROM}`,
-        to: `${process.env.EMAIL_FROM}`,
-        replyTo: body.email,
-        subject: `New Contact Form Submission: ${body.subject}`,
-        html: `
-        <h2>New Contact Form Submission</h2>
-        
-        <p><strong>Name:</strong> ${body.name}</p>
-        <p><strong>Email:</strong> ${body.email}</p>
-        <p><strong>Subject:</strong> ${body.subject}</p>
-        
-        <h3>Message:</h3>
-        <p>${body.message.replace(/\n/g, '<br>')}</p>
-        `,
+        const message = {
+            from: `${process.env.EMAIL_FROM}`,
+            to: `${process.env.EMAIL_FROM}`,
+            replyTo: body.email,
+            subject: `New Contact Form Submission: ${body.subject}`,
+            html: `
+            <h2>New Contact Form Submission</h2>
+            
+            <p><strong>Name:</strong> ${body.name}</p>
+            <p><strong>Email:</strong> ${body.email}</p>
+            <p><strong>Subject:</strong> ${body.subject}</p>
+            
+            <h3>Message:</h3>
+            <p>${body.message.replace(/\n/g, '<br>')}</p>
+            `,
 
-        headers: {
-            "X-Entity-Ref-ID": "contact-form",
-        },
-        
-    };
+            headers: {
+                "X-Entity-Ref-ID": "contact-form",
+            },
+        };
 
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_FROM,
-            pass: process.env.PASS,
-        },
-        tls: {
-            rejectUnauthorized: false,
-        },
-    });
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_FROM,
+                pass: process.env.PASS,
+            },
+            tls: {
+                rejectUnauthorized: false,
+            },
+        });
 
-    console.log(JSON.stringify(body))
+        console.log(JSON.stringify(body));
 
-    try {
         await transporter.sendMail(message);
-        return NextResponse.json({ message: "Email sent successfully"}, { status: 200 });
+        return NextResponse.json({ message: "Email sent successfully" }, { status: 200 });
     } catch (error: any) {
         console.error("Error sending email:", error);
         return NextResponse.json({ message: "Failed to send email" }, { status: 500 });
